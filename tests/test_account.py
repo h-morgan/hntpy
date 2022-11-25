0d259f8cdf5f2511ef8ffc6ea251f114a7313e1f
Haley Morgan <haleymorgan3264@gmail.com>
2022-11-25 15:55:34 -0500

    add more tests for account functions

--- tests/test_account.py
+++ tests/test_account.py
@@ -1,31 +1,35 @@
 from hntpy import Account
+from types import GeneratorType
+
 
 def test_hnt_py():
     account_id = "1234"
     account = Account(account_id)
     assert account.get_account_id() == account_id
 
+
 def test_account_id():
     account_id = "1234"
     account = Account(account_id)
     assert account.get_account_id() == account_id
 
+
 def test_account_details():
     account_id = "13d5xg6qzdE2sVtep6GtbYtJ3fPCvxwpjMWSD4L7hBtSVrrjfZR"
     account = Account(account_id)
     details = account.get_account_details()
-    
-    assert "data" in details
-    assert "address" in details["data"]
-    
+
+    assert "address" in details
+
     # get address returned from API response, should match our original account address
-    api_addr = details["data"]["address"]
+    api_addr = details["address"]
     assert api_addr == account_id
+
 
 def test_get_hotspots():
     account = Account(account_id="13d5xg6qzdE2sVtep6GtbYtJ3fPCvxwpjMWSD4L7hBtSVrrjfZR")
     details = account.hotspots()
-    
+
     assert isinstance(details, list)
 
 
@@ -41,16 +45,30 @@ def test_get_hotspots_with_filter():
     # test invalid filter
     filter_mode = "bad"
     details = account.hotspots(filter_mode=filter_mode)
-    
+
     assert isinstance(details, list)
+
 
 def test_get_validators():
     account = Account(account_id="13d5xg6qzdE2sVtep6GtbYtJ3fPCvxwpjMWSD4L7hBtSVrrjfZR")
     details = account.validators()
-    
+
     assert isinstance(details, list)
 
-    
+
+def test_get_ouis():
+    account = Account(account_id="13d5xg6qzdE2sVtep6GtbYtJ3fPCvxwpjMWSD4L7hBtSVrrjfZR")
+    details = account.ouis()
+
+    assert isinstance(details, list)
+
+    # get generator
+    gen = account.roles(gen=True)
+    assert isinstance(gen, GeneratorType)
 
 
+def test_get_roles():
+    account = Account(account_id="1d5xg6qzdE2sVtep6GtbYtJ3fPCvxwpjMWSD4L7hBtSVrrjfZR")
+    details = account.roles()
 
+    assert isinstance(details, list)
