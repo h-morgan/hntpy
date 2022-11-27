0c6ff0db3b74a06b3c34142504ba52ecc46aab6b
Haley Morgan <haleymorgan3264@gmail.com>
2022-11-26 22:48:17 -0500

    reconfigure HeliumClient to simplify requests, move URL building to Account class

--- hntpy/account.py
+++ hntpy/account.py
@@ -1,9 +1,11 @@
 from hntpy.helium_client import HeliumClient
+from typing import Union
 
 
 class Account:
 
     client = HeliumClient()
+    base_url = "accounts"
 
     def __init__(self, account_id: str):
         self.account_id = account_id
@@ -12,54 +14,118 @@ class Account:
         return self.account_id
 
     def get_account_details(self) -> dict:
-        return self.client.get_account_data(self.account_id)
+        url = self.base_url + f"/{self.account_id}"
+        return self.client.get_data(url)
 
-    def hotspots(self, filter_mode: str = None) -> list:
+    def hotspots(self, filter_modes: str = None) -> list:
         """Get a list of hotspots and their details for the account
 
         filter_mode is an optional parameter, which can be one or more of the following options:
         - full,dataonly,light
         """
         # only possible combinations of filter_mode param (per Helium API docs)
-        filter_modes = [
-            "full",
-            "dataonly",
-            "light",
-            "full,dataonly,light",
-            "full,dataonly" "full,light",
-            "dataonly,light",
-        ]
-        if filter_mode in filter_modes:
-            param = {"filter_mode": filter_mode}
+        if filter_modes:
+            params = {"filter_modes": filter_modes}
         else:
-            param = {}
-        data = self.client.get_account_data(
-            self.account_id, suffix="hotspots", params=param
-        )
+            params = {}
+
+        url = self.base_url + f"/{self.account_id}/hotspots"
+        data = self.client.get_data(url, params=params)
 
         return data
 
     def validators(self) -> list:
         """Get a list of validators and their details for the account"""
-        data = self.client.get_account_data(self.account_id, suffix="validators")
+        url = self.base_url + f"/{self.account_id}/validators"
+        data = self.client.get_data(url)
         return data
 
-    def ouis(self, gen=False) -> list:
-        """Get a list of ouis owned by an account. Can optionally return data as a generator."""
+    def ouis(self, gen: bool = False) -> list:
+        """Get a list of all ouis owned by an account. Can optionally return data as a generator."""
+        url = self.base_url + f"/{self.account_id}/ouis"
         if gen:
-            data = self.client.get_account_gen(self.account_id, suffix="ouis")
+            data = self.client.gen_data(url)
         else:
-            data = self.client.get_account_data(self.account_id, suffix="ouis")
+            data = self.client.get_data(url)
         return data
 
-    def roles(self, gen=False) -> list:
-        """Gets list of transactions that indicate an account as a participant. Can optionally return data as a generator."""
+    def roles(self, gen: bool = False) -> list:
+        """Gets list of all roles (transactions) that indicate an account as a participant. Can optionally return data as a generator."""
+        url = self.base_url + f"/{self.account_id}/roles"
         if gen:
-            data = self.client.get_account_gen(self.account_id, suffix="roles")
+            data = self.client.gen_data(url)
         else:
-            data = self.client.get_account_data(self.account_id, suffix="roles")
+            data = self.client.get_data(url)
         return data
 
-    def role_counts(self) -> dict:
-        data = self.client.get_account_data(self.account_id, suffix="roles/count")
+    def role_counts(self, filter_types: str = None) -> dict:
+        """Gets counts of all role (transaction) types.
+
+        filter_types is an optional parameter, which can should be a comma-separated list (wihtout spaces) of one or more of a transaction type.
+        Example: filter_types="gen_gateway_v1,poc_request_v1"
+
+        If no filter_types arg provided, counts for all role types are returned.
+        """
+        url = self.base_url + f"/{self.account_id}/roles/count"
+        if filter_types:
+            params = {"filter_types": filter_types}
+        else:
+            params = {}
+        data = self.client.get_data(url, params=params)
+        return data
+
+    def rewards(
+        self, max_time: str = None, min_time: str = None, gen: bool = False
+    ) -> list:
+        """Gets all rewards for an account, in a given timebound between max_time and min_time
+
+        Notes:
+            - max_time and min_time should be date strings, in the format "YYYY-mm-dd".
+            - The block that contains the `max_time` stamp is excluded from result (upper bound not inclusive)
+            - max_time and min_time both default to `now`, so at least one bound should be provided
+            - timestamps are returned in UTC
+        """
+        url = self.base_url + f"/{self.account_id}/rewards"
+
+        params = {}
+        if max_time:
+            params["max_time"] = max_time
+        if min_time:
+            params["min_time"] = min_time
+
+        if gen:
+            data = self.client.gen_data(url, params=params)
+        else:
+            data = self.client.get_data(url, params=params)
+
+        return data
+
+    def rewards_sum(
+        self, max_time: str = None, min_time: str = None, bucket: str = None
+    ) -> Union[dict, list]:
+        """Gets rewards totals for an account. Time range optional, between max_time and min_time. Bucket can optionally can group rewards.
+
+        Notes:
+            - max_time and min_time should be ISO date strings, in the format "YYYY-mm-dd".
+            - valid bucket values = "hour", "day", or "week"
+            - The block that contains the `max_time` stamp is excluded from result (upper bound not inclusive)
+            - when timestmaps are omitted, current time is assumed
+            - if bucket param provided, a list of rewards sums broken up by requested bucket-length is returns
+
+        Returns (2 cases):
+            dict: if no bucket arg provided
+            list: if bucket provided
+        """
+        url = self.base_url + f"/{self.account_id}/rewards/sum"
+
+        params = {}
+        if max_time:
+            params["max_time"] = max_time
+        if min_time:
+            params["min_time"] = min_time
+        if bucket:
+            params["bucket"] = bucket
+
+        data = self.client.get_data(url, params=params)
+
         return data
