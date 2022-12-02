88021b16bc3520a7ba34cd0b6352f6d2b869f0c5
Haley Morgan <haleymorgan3264@gmail.com>
2022-12-01 22:37:58 -0500

    redistribute resources in __init__ files

--- hntpy/__init__.py
+++ hntpy/__init__.py
@@ -1,7 +1,5 @@
-from hntpy.resources.account import Account
-from hntpy.resources.hotspot import Hotspot
-from hntpy.resources.validator import Validator
-from hntpy.network.stats import *
+from hntpy.resources import *
+from hntpy.network import *
 from loguru import logger
 import sys
 
---
+++ hntpy/network/__init__.py
@@ -0,0 +1,2 @@
+from hntpy.network.stats import *
+from hntpy.network.oracle import *
