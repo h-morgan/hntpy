52a5fa091695557e00206eab792acd7c030b440c
Haley Morgan <haleymorgan3264@gmail.com>
2022-12-01 22:29:50 -0500

    create stats requests and tests

--- hntpy/__init__.py
+++ hntpy/__init__.py
@@ -1,6 +1,7 @@
 from hntpy.resources.account import Account
 from hntpy.resources.hotspot import Hotspot
 from hntpy.resources.validator import Validator
+from hntpy.network.stats import *
 from loguru import logger
 import sys
 
