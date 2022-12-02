f4f8a42be21b62922e5a0336a8252e4b16bbdf12
Haley Morgan <haleymorgan3264@gmail.com>
2022-12-01 22:33:58 -0500

    add type hints to stats.py

--- hntpy/network/stats.py
+++ hntpy/network/stats.py
@@ -1,4 +1,5 @@
 from hntpy.helium_client import HeliumClient
+from typing import Union
 
 # for these requests, we don't want to have to instantiate a class to be able to call methods,
 # so keep these functions at the file level
@@ -7,14 +8,19 @@ client = HeliumClient()
 BASE_URL = "stats"
 
 
-def get_blockchain_stats():
+def get_blockchain_stats() -> dict:
     """Retrieve stats for the blockchain"""
     url = BASE_URL
     return client.get_data(url)
 
 
-def get_token_supply(format="json"):
-    """Returns the circulating token supply"""
+def get_token_supply(format: str = "json") -> Union[dict, str]:
+    """Returns the circulating token supply
+
+    Notes:
+        - `format` arg can either be "json" or "raw"
+        - if "raw" format requested, returns a string value of the token supply number
+    """
     url = BASE_URL + "/token_supply"
 
     # validate format value by turning it to all lowercase, just in case
